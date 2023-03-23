00e6ae1ab34da1ca0e85af6699a0daa9cffc27c5
ynskrca12 <ynskrca52@outlook.com>
2023-03-24 02:56:10 +0300

    Display all invoice and Search Invoice

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         //\App\Models\Counter::factory(1)->create();
+        \App\Models\Customer::factory(10)->create();
         //\App\Models\Product::factory(1)->create();
         //\App\Models\Invoice::factory(5)->create();
        // \App\Models\InvoiceItem::factory(5)->create();
